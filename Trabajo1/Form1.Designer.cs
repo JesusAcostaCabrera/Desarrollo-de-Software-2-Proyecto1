19c9436d189b3403a60893bf0739b0420c64c756
Reshuo <69280892+Reshuo@users.noreply.github.com>
2022-05-11 23:51:55 -0400

    Subida de Proyecto
    Aqui estan todos los archivos del proyecto

---
+++ Trabajo1/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Trabajo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSueldo = new System.Windows.Forms.TextBox();
+            this.txtISR = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAFP = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSSF = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(73, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Calculo de Impuestos";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(137, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Sueldo";
+            // 
+            // txtSueldo
+            // 
+            this.txtSueldo.Location = new System.Drawing.Point(113, 73);
+            this.txtSueldo.Name = "txtSueldo";
+            this.txtSueldo.Size = new System.Drawing.Size(100, 20);
+            this.txtSueldo.TabIndex = 2;
+            // 
+            // txtISR
+            // 
+            this.txtISR.Location = new System.Drawing.Point(12, 134);
+            this.txtISR.Name = "txtISR";
+            this.txtISR.ReadOnly = true;
+            this.txtISR.Size = new System.Drawing.Size(63, 20);
+            this.txtISR.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 17);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "ISR";
+            // 
+            // txtAFP
+            // 
+            this.txtAFP.Location = new System.Drawing.Point(126, 134);
+            this.txtAFP.Name = "txtAFP";
+            this.txtAFP.ReadOnly = true;
+            this.txtAFP.Size = new System.Drawing.Size(63, 20);
+            this.txtAFP.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(126, 114);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 17);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "AFP";
+            // 
+            // txtSSF
+            // 
+            this.txtSSF.Location = new System.Drawing.Point(248, 134);
+            this.txtSSF.Name = "txtSSF";
+            this.txtSSF.ReadOnly = true;
+            this.txtSSF.Size = new System.Drawing.Size(63, 20);
+            this.txtSSF.TabIndex = 8;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(248, 114);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 17);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "SSF";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(13, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(320, 39);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(352, 286);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSSF);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtAFP);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtISR);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtSueldo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSueldo;
+        private System.Windows.Forms.TextBox txtISR;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtAFP;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtSSF;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Trabajo1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabajo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Calcular()
+        {
+
+            double Sueldo = double.Parse(txtSueldo.Text);
+
+            txtISR.Text = Math.Round(Sueldo * 0.09,2).ToString();
+
+            txtAFP.Text = Math.Round(Sueldo * 0.07,2).ToString();
+
+            txtSSF.Text = Math.Round(Sueldo * 0.10,2).ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Calcular();
+        }
+    }
+}
